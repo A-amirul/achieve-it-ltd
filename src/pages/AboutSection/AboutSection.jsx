816e071349bb6f4371a55d35a8a1d0ca13cf37ec
A-amirul <amirulislamamir412@gmail.com>
2023-08-10 15:13:03 +0600

    update home

--- src/pages/AboutSection/AboutSection.jsx
+++ src/pages/AboutSection/AboutSection.jsx
@@ -6,14 +6,17 @@ import OurStory from "./OurStory";
 const AboutSection = () => {
   return (
     <div className=" mx-auto max-w-screen-xl">
-      <h1 className="text-5xl mt-16 mb-8 font-bold ">ABOUT US</h1>
+      <h1 className="text-4xl mt-16 mb-8 font-semibold ">ABOUT US</h1>
       <About></About>
-      <h1 className="text-5xl mt-16 mb-8 font-bold text-center">Our Story</h1>
+      <h1 className="text-5xl mt-16 mb-8 font-semibold text-center">Our Story</h1>
+      <p className="text-slate-500 text-center my-2 font-medium">
+        Know about Us
+      </p>
       <OurStory></OurStory>
       <CEOSection></CEOSection>
       <OurGoal></OurGoal>
       <div className="mt-16 mb-10">
-        <h1 className="text-5xl font-bold px-5 text-center">
+        <h1 className="text-5xl font-semibold px-5 text-center">
           {" "}
           LEADERSHIP TEAM{" "}
         </h1>
