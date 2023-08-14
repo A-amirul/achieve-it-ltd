e0ff145ce497d7c516901be1529bf50ebb45d038
A-amirul <amirulislamamir412@gmail.com>
2023-08-14 17:49:20 +0600

    header padding margin changed

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -3,44 +3,56 @@ import logo from "../../assets/logo/logo.png";
 const Header = () => {
   const navOptions = (
     <>
-      <li>
-        <Link to={`http://localhost:5173/`}>Home</Link>
-      </li>
+      <div className="md:flex items-center">
+        <div className="md:mr-[10px]">
+          <li>
+            <a>
+              <img className="w-24 h-20" src={logo} alt="" />
+            </a>
+          </li>
+        </div>
 
-      <li>
-        <Link to={`http://localhost:5173/about`}>About Us</Link>
-      </li>
-      <li>
-        <details className="dropdown dropdown-hover">
-          <summary tabIndex={0}>Company</summary>
-          <ul
-            tabIndex={0}
-            className="dropdown-content z-[1] menu mt-28 shadow bg-blue-600 text-white rounded-box w-52"
-          >
-            <li>
-              <a>Gallery</a>
-            </li>
-            <li>
-              <a>Teams</a>
-            </li>
-            <li>
-              <a>Testimonials</a>
-            </li>
-            <li>
-              <a>Pricing</a>
-            </li>
-          </ul>
-        </details>
-      </li>
-      <li>
-        <Link to="/blog">Blog Section</Link>
-      </li>
-      <li>
-        <Link to={`http://localhost:5173/contact`}>Contact US</Link>
-      </li>
-      <li>
-        <Link to="/ComingSoon">Career</Link>
-      </li>
+        <div className="md:flex md:mx-[300px]">
+          <li>
+            <Link to={`http://localhost:5173/`}>Home</Link>
+          </li>
+
+          <li>
+            <Link to={`http://localhost:5173/about`}>About Us</Link>
+          </li>
+          <li>
+            <details className="dropdown dropdown-hover">
+              <summary tabIndex={0}>Company</summary>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu shadow bg-blue-600 text-white rounded-box w-52"
+              >
+                <li>
+                  <a>Gallery</a>
+                </li>
+                <li>
+                  <a>Teams</a>
+                </li>
+                <li>
+                  <a>Testimonials</a>
+                </li>
+                <li>
+                  <a>Pricing</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <Link to="/blog">Blog Section</Link>
+          </li>
+          <li>
+            <Link to={`http://localhost:5173/contact`}>Contact US</Link>
+          </li>
+          <li>
+            <Link to="/ComingSoon">Career</Link>
+          </li>
+        </div>
+      </div>
     </>
   );
 
@@ -66,14 +78,11 @@ const Header = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-500 text-base-100 rounded-box w-52"
           >
             {navOptions}
           </ul>
         </div>
-        <a className=" ">
-          <img className="w-24 h-24" src={logo} alt="" />
-        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 text-base-100">
