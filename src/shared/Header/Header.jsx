b8b1a6c1cd31b5c0c7bc66063685b2465a391536
A-amirul <amirulislamamir412@gmail.com>
2023-08-14 17:00:38 +0600

    Header section added

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -11,15 +11,26 @@ const Header = () => {
         <Link to={`http://localhost:5173/about`}>About Us</Link>
       </li>
       <li>
-      <details className="dropdown ">
-  <summary className=" ">Company</summary>
-  <ul className="p-2 shadow menu dropdown-content absolute z-[1] bg-base-100 rounded-box w-52">
-    <li><a>Gallary</a></li>
-    <li><a>Testimonials</a></li>
-    <li><a>Teams</a></li>
-    <li><a>Pricings</a></li>
-  </ul>
-</details>
+        <details className="dropdown dropdown-hover">
+          <summary tabIndex={0}>Company</summary>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu mt-28 shadow bg-blue-600 text-white rounded-box w-52"
+          >
+            <li>
+              <a>Gallery</a>
+            </li>
+            <li>
+              <a>Teams</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+          </ul>
+        </details>
       </li>
       <li>
         <Link to="/blog">Blog Section</Link>
@@ -34,51 +45,42 @@ const Header = () => {
   );
 
   return (
-    <div className="bg- w-full">
-      <div className=" ">
-        <div className="navbar  max-w-screen-xl mx-auto z-10  text-black px-8">
-          <div className="navbar-start">
-            <div className="dropdown px-2">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 "
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 hover:bg-base-200 text-black rounded-box w-52 font-medium"
-              >
-                {navOptions}
-              </ul>
-            </div>
-            <Link to="/" className="font-bold text-xl">
-              <img
-                className="mx-auto bg-blue-900"
-                style={{ width: "100px", height: "80px" }}
-                src={logo}
-                alt=""
+    <div className="navbar bg-blue-500  font-semibold text-lg">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
               />
-            </Link>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-2  font-semibold">
-              {navOptions}
-            </ul>
-          </div>
-          <div className="navbar-end"></div>
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navOptions}
+          </ul>
         </div>
+        <a className=" ">
+          <img className="w-24 h-24" src={logo} alt="" />
+        </a>
       </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-base-100">
+          {navOptions}
+        </ul>
+      </div>
+      <div className="navbar-end">{/* <a className="btn">Button</a> */}</div>
     </div>
   );
 };
